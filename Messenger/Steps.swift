bdf239f73307fc35416b5116922837de34a098ab
ASHISH YADAV <ashishydv249@gmail.com>
2020-11-15 23:47:58 +0530

    Building User Interface & Dependencies

--- Messenger/Steps.swift
+++ Messenger/Steps.swift
@@ -11,30 +11,15 @@ import Firebase
 //------------------------------------------------------------------------------------------
 //------------------------------------------------------------------------------------------
 //MARK: Previous Part
-//MARK: Part 5 : Database Setup
+//MARK: Part : main
 //------------------------------------------------------------------------------------------
 //------------------------------------------------------------------------------------------
-//1. Added CocoaLumberjack for console output of the app flow : LogManager.Swift
-//2. UserDefault manager added : UDManager.swift
-//3. Setup Firebase Database
-//    -> goto firebase -> Left Panel -> Realtime database -> Create Database
-//    -> Start in test Mode
-//    -> got to rule tab and set read and write permission == true
-//4. Create global api calls
-//    -> Register new user in Database
-//    -> Check user is already registered or not
-//    -> User Sign In
 
 
 //------------------------------------------------------------------------------------------
 //------------------------------------------------------------------------------------------
 //MARK: Current Part
-//MARK: Part 6 : Facebook Login & Log Out (Real-time)
+//MARK: Part 8 : Building User Interface & Dependencies
 //------------------------------------------------------------------------------------------
 //------------------------------------------------------------------------------------------
-//1. Setup Conversation Screen
-//2. Setup Logout button
-//3.
-
-
-
+//1. 
